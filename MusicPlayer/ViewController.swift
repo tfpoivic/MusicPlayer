7b7b9eeca0cb4293fafb9d691028c12eb8b5905a
tfpoivic <tfpoivic@gmail.com>
2020-01-05 16:39:16 +0800

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by 莊宇軒 on 2020/1/5.
+//  Copyright © 2020 kffatkalo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
